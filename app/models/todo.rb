55610e0dcadf94b4225b6feab31e101e68f5ead1
Melanie <melanie-melimela@hotmail.com>
2023-11-29 12:44:08 +0100

    normalize and validate - spec for the model

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,6 @@
 class Todo < ApplicationRecord
-  # TODO 1
-  # TODO 2
+  # TODO 1 No blank data
+  validates :title, presence: true
+  # TODO 2 leading and trailing white spaces are removed
+  normalizes :title, with: -> title {title.strip}
 end
