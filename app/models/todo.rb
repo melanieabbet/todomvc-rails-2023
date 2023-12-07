b7c3b9206daa3da9c0add2ffa0c525db6207b79a
Melanie <melanie-melimela@hotmail.com>
2023-12-07 20:57:59 +0100

    add toogle

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -3,10 +3,18 @@ class Todo < ApplicationRecord
   scope :completed, -> { where(completed: true) }
 
   validates :title, presence: true
-
   normalizes :title, with: ->(title) { title.strip }
 
   def self.toggle_all!
-    # TODO
+
+    update_all completed: active.any?
+
+    # same
+    # if active.any?
+    #   update_all completed: true
+    # elsif
+    #   update_all completed: false
+    # end 
+
   end
 end
